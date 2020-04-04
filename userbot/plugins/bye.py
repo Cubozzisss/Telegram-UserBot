f1f30e6e257a3211ec8c4960001fb36b13c3e3e9
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-04 03:00:05 +0200

    Comando .bye tradotto testo in IT

--- userbot/plugins/bye.py
+++ userbot/plugins/bye.py
@@ -26,7 +26,7 @@ async def leave(e):
 
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
 
-        await e.edit("`I am leaving this chat.....!`")
+        await e.edit("`Esco dalla chat!`")
 
         time.sleep(3)
 
@@ -36,4 +36,4 @@ async def leave(e):
 
         else:
 
-            await e.edit('`Sir This is Not A Chat`')
+            await e.edit('`Non puoi uscire dalla chat`')
