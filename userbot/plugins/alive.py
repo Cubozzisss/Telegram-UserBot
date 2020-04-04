8ee381f5250e901e380c9a411896d44d4e740ac4
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-04 02:57:42 +0200

    Modifica testo e traduzione IT

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check p
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @A_Dark_Princ3\n"
-                     "`Bot created by:` [SnapDragon](tg://user?id=719877937)\n"
-                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
+    await alive.edit("**`∇ STATUS USERBOT ∇`**\n\n"
+                     "`Versione Telethon: 7.0.1\nVersione Python: 3.8.0\nRAMO:` @IOIIOIIIOIIOI\n"
+                     "`CREATORE BOT:` [@SnapDragon7410](tg://user?id=719877937)\n"
+                     "`STATUS DATABASE: In esecuzione...\n\nIl mio master\n`"
+                     f"`PROPIETARIO`: {DEFAULTUSER}\n"
+                     "[DISTRIBUISCI QUESTO USERBOT](https://github.com/IIOOIIOIIOOII/USERBOT-ITA.git)")
