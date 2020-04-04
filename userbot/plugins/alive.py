d5f7097a80cb81f7f7796983fd7fe3323d81182a
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-04 05:28:19 +0200

    Aggiornamento testo.

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,14 +6,14 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "senza nome"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     await alive.edit("**`∇ STATUS USERBOT ∇`**\n\n"
                      "`Versione Telethon: 7.0.1\nVersione Python: 3.8.0\nRAMO:` @IOIIOIIIOIIOI\n"
-                     "`CREATORE BOT:` [@SnapDragon7410](tg://user?id=719877937)\n"
+                     "`CREATORE BOT:` [SnapDragon7410](tg://user?id=719877937)\n"
                      "`STATUS DATABASE: In esecuzione...\n\nIl mio master\n`"
                      f"`PROPIETARIO`: {DEFAULTUSER}\n"
                      "[DISTRIBUISCI QUESTO USERBOT](https://github.com/IIOOIIOIIOOII/USERBOT-ITA.git)")
