70ef099fecac29840609876a5462fab384180067
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-05 08:52:08 +0200

    Inserito nel bot

--- userbot/plugins/spam.py
+++ userbot/plugins/spam.py
@@ -7,11 +7,10 @@
 from asyncio import wait
 
 from telethon import events
-
 from userbot.utils import admin_cmd
 
 
-@borg.on(events.NewMessage(pattern=r"\.spam", outgoing=True))
+@borg.on(admin_cmd("spam"))
 async def spammer(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
         message = e.text
