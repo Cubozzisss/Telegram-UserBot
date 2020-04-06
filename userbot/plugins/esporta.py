f12264bbc0dde598596d438d449830f81d965d6a
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-06 23:20:58 +0200

    Fix3 FORSE

--- userbot/plugins/esporta.py
+++ userbot/plugins/esporta.py
@@ -12,9 +12,8 @@ from userbot.utils import admin_cmd
 import asyncio
 
 
-
-
-@borg.on(admin_cmd(pattern=r("esporta"), outgoing=True)
+@borg.on(admin_cmd(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+)
 
 async def _(event):
 
