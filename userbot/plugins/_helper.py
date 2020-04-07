97c6ea8e9ce6328fd3cde9882a9e89268c126436
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-07 20:23:27 +0200

    Aggionamento menù

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -28,8 +28,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. Provided by @A_Dark_Princ3 \n[Check out this dope af website](https://www.moddingunited.xyz/) \n
-`Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
+            help_string = """Userbot Helper.. Fornito da @IOIIIOIIIOI \n\n
+`Userbot Helper per i comandi`\n__Premi .help plugin_name for, in caso sia qui.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
