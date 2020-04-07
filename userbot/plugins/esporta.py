9dfdbad9e057000aa09317ea34a1673db4e3da2b
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-07 02:01:41 +0200

    fix5

---
+++ userbot/plugins/esporta.py
@@ -0,0 +1,56 @@
+#Credit: @r4v4n4
+"""Emoji
+
+Available Commands:
+
+.esporta"""
+
+from telethon import events
+
+import asyncio
+
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+)
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 1
+
+    animation_ttl = range(0, 17)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "esporta":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+        
+            "⬛⬛⬛\n⬛⬛⬛\n⬛⬛⬛",
+            "⬛⬛⬛\n⬛🔄⬛\n⬛⬛⬛",
+            "⬛⬆️⬛\n⬛🔄⬛\n⬛⬛⬛",
+            "⬛⬆️↗️\n⬛🔄⬛\n⬛⬛⬛",
+            "⬛⬆️↗️\n⬛🔄➡️\n⬛⬛⬛",    
+            "⬛⬆️↗️\n⬛🔄➡️\n⬛⬛↘️",
+            "⬛⬆️↗️\n⬛🔄➡️\n⬛⬇️↘️",
+            "⬛⬆️↗️\n⬛🔄➡️\n↙️⬇️↘️",
+            "⬛⬆️↗️\n⬅️🔄➡️\n↙️⬇️↘️",
+            "↖️⬆️↗️\n⬅️🔄➡️\n↙️⬇️↘️",
+            "**Esportazione della chat** `./Inpu/`",
+            "**Esportazione della chat** `./Inpu/homework/`",
+            "**Esportazione della chat** `./Inpu/homework/groupchat.txt`",
+            "__Abbandono della chat.....! Bye..__",
+            "__Abbandono della chat.....! Bye..__"
+
+ ]
+
+        for i in animation_ttl:
+
+            await asyncio.sleep(animation_interval)
+
+            await event.edit(animation_chars[i % 17])
