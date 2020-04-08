c177d067685743b3a6ccbbf1b07c9b971a23e2f3
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-08 05:02:59 +0200

    Add files via upload

--- userbot/plugins/plane.py
+++ userbot/plugins/plane.py
@@ -3,8 +3,11 @@ from telethon import events
 import asyncio
 import os
 import sys
+from userbot import CMD_HELP 
 
+from userbot.utils import admin_cmd
 
+@borg.on(admin_cmd(pattern=f"plane", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.plane", outgoing=True))
 async def _(event):
     if event.fwd_from:
