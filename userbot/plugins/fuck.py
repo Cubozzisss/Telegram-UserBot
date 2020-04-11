11e5131a1060d87006ecbb7fb2075d6d137c99d5
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-11 05:28:24 +0200

    Caricato in help

--- userbot/plugins/fuck.py
+++ userbot/plugins/fuck.py
@@ -8,14 +8,16 @@ Available Commands:
 
 .kiss"""
 
+
 from telethon import events
+
+from userbot.utils import admin_cmd
 
 import asyncio
 
 
 
-
-
+@borg.on(admin_cmd(pattern=f"fuck", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
@@ -53,6 +55,7 @@ async def _(event):
             await event.edit(animation_chars[i % 4])
 
 
+@borg.on(admin_cmd(pattern=f"sex", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
@@ -90,17 +93,7 @@ async def _(event):
             await event.edit(animation_chars[i % 4])
 
 
-""
-
-
-from telethon import events
-
-import asyncio
-
-
-
-
-
+@borg.on(admin_cmd(pattern=f"kiss", allow_sudo=True))
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
 
 async def _(event):
