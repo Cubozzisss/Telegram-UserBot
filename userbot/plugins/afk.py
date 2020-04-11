8f911a4c90cc4b2f1b9637d7256a00bdbae9346a
IIOOIIOIIOOII <62676403+IIOOIIOIIOOII@users.noreply.github.com>
2020-04-11 04:10:50 +0200

    fix testo

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -1,11 +1,12 @@
 """AFK Plugin for @UniBorg
 Syntax: .afk REASON"""
+
 import asyncio
 import datetime
 from datetime import datetime
+
 from telethon import events
 from telethon.tl import functions, types
-from uniborg.util import admin_cmd
 
 global USER_AFK  # pylint:disable=E0602
 global afk_time  # pylint:disable=E0602
@@ -17,7 +18,7 @@ afk_time = None
 last_afk_message = {}
 afk_start = {}
 
-@borg.on(admin_cmd(pattern="afk"))
+
 @borg.on(events.NewMessage(pattern=r"\.afk ?(.*)", outgoing=True))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
@@ -45,15 +46,15 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await borg.send_message(event.chat_id, f"**Yoah, Ora vado AFK⛔️** __MOTIVO ~ {reason}__")
+            await borg.send_message(event.chat_id, f"**Fuck, Ora vado AFK ⛔️** __MOTIVO ~ {reason}__")
         else:
-            await borg.send_message(event.chat_id, f"**Yoah, Ora sono AFK!**")
+            await borg.send_message(event.chat_id, f"**Fuck, Ora sono AFK!**")
         await asyncio.sleep(5)
         await event.delete()
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
-                f"IMPOSTA LA FUNZIONE AFK IN True, and Reason is {reason}"
+                f"MODE AFK ON 🖕 {reason}"
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             logger.warn(str(e))  # pylint:disable=E0602
@@ -72,11 +73,11 @@ async def set_not_afk(event):
         total_afk_time = str((afk_end - afk_start))
     current_message = event.message.message
     if ".afk" not in current_message and "yes" in USER_AFK:  # pylint:disable=E0602
-        shite = await borg.send_message(event.chat_id, "__Yoah, Ora non sono più AFK!__\n**Ora puoi scrivermi (:**\n `Sono stato afk per:``" + total_afk_time + "`")
+        shite = await borg.send_message(event.chat_id, "__Fuck, non sono più AFK!__\n**Ora puoi scrivermi.**\n`Sono stato afk per:``" + total_afk_time + "`")
         try:
             await borg.send_message(  # pylint:disable=E0602
                 Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
-                "IMPOSTA LA FUNZIONE AFK IN False"
+                "MODE AFK OFF 🖕 "
             )
         except Exception as e:  # pylint:disable=C0103,W0703
             await borg.send_message(  # pylint:disable=E0602
@@ -145,10 +146,10 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"__Il mio mastro è stato ⛔️OFF⛔️ per__ `{total_afk_time}`\nLui è Un cavolo: ~~DI DIO~~ " + \
-            f"\n\n__Te lo prometto Io Tornero tra 20 Anni luce__\n**MOTIVO**: {reason}" \
+        message_to_reply = f"__⛔️ IL MIO CAPO E OFF ⛔️ DA__ `{total_afk_time}`\nNON ROMPERE  " + \
+            f"\n\n__PROMETTO CHE TORNO PRESTO BITCH 🖕__\n**MOTIVO**: {reason}" \
             if reason \
-            else f"**⛔️AL MOMENTO SONO OFF⛔️.**\n__.QUINDI NON ROMPERE IL CAZZO Spammando GRAZIE__\nAppena torno cercherò di rispondere. Grazie per avermi contattato __Come ho detto ti risponderò il prima possibile, BYE__**👀**  "
+            else f"**⛔️ AL MOMENTO SONO OFF ⛔️**\n__QUINDI NON ROMPERE IL CAZZO SPAMMANDO GRAZIE ‼️__\nAPPENA TORNO CERCO DI RISPONDERE A TUTTI BYE ** 👍 **  "
         msg = await event.reply(message_to_reply)
         await asyncio.sleep(5)
         if event.chat_id in last_afk_message:  # pylint:disable=E0602
