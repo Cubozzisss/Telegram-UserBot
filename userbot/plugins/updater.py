fb90e4195298836ced927903ee98298c62e8cbee
AlpHyx <61375258+AlpHyx74@users.noreply.github.com>
2020-04-12 17:31:56 +0200

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -44,7 +44,7 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/IIOOIIOIIOOII/USERBOT-ITA"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/AlpHyx74/USERBOT-ITA/edit/"
 BOT_IS_UP_TO_DATE = "`Update in corso.\nGrazie per aver utilizzato il servizio.`"
 NEW_BOT_UP_DATE_FOUND = (
     "new update found {branch_name}\n"
