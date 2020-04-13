2cc06b32aaed6aad0a71694848e6bab356fa8c8b
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-04-13 19:22:53 +0200

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -11,9 +11,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "senza nome"
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`⚡∇⚡ STATUS USERBOT ⚡∇⚡`**\n\n"
-                     "`-Versione Telethon: 7.0.1\n-Versione Python: 3.8.0\n🔸UPDATE:` @AnonHexo\n"
-                     "`🔸CREATORE BOT:` [AnonHexo](tg://user?id=304506948)\n"
-                     "`-CPU: In esecuzione...\n\n⚡∇⚡ DATI USER ⚡∇⚡\n`"
-                     f"`-USER`: {DEFAULTUSER}\n"
-                     "🔸[REPOSITORY USERBOT](https://github.com/AlpHyx74)")
+    await alive.edit("**🎲 __ANONHEXO USERBOT STATUS__ 🎲**\n\n"
+                     "🔹Tetethon Version: 7.0.1\n🔹Python Version: 3.8.0\n🔸**SUPPORT & UPDATE: @AnonHexoUserBot **\n"
+                     "🔸**BOT CREATOR:** [AnonHexo](tg://user?id=304506948) **\n"
+                     "🔹**CPU:** Ok \n\n🎲 **__USER DATA__** 🎲\n"
+                     f"🔸**USER:** {DEFAULTUSER}\n"
+                     "🔸[REPOSITORY USERBOT](https://github.com/AnonHexo)")
