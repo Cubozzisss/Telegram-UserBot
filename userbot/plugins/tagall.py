2efbc791d982e492e6aa59658d561131596b9059
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-04-13 19:34:15 +0200

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -9,7 +9,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "@tagall"
+    mentions = "@tagall __by [AnonHexoBot](https://github.com/AnonHexo/Telegram-UserBot)__"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
         mentions += f"[\u2063](tg://user?id={x.id})"
