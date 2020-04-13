f891b256cf4d5c158cf3901e23ae6d2f8cac8a4c
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-04-13 20:22:02 +0200

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -3,8 +3,12 @@ Syntax: .afk REASON"""
 
 import asyncio
 import datetime
-from userbot import ALIVE_NAME
 from datetime import datetime
+import io
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon import events, errors, functions, types
+from userbot import ALIVE_NAME, LESS_SPAMMY
+from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**Nessun nome selezionato, controlla su @AnonHexoUserBot"
 
