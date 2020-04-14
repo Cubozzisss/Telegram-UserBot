b87a1c030567fb272d7be493b0ba4472a2721bc6
AnonHexo <61375258+AnonHexo@users.noreply.github.com>
2020-04-14 13:28:04 +0200

    Update stats.py

--- userbot/plugins/stats.py
+++ userbot/plugins/stats.py
@@ -38,9 +38,9 @@ async def _(event):
     end = datetime.now()
     ms = (end - start).seconds
     await event.edit("""`Your Stats Obtained in {} seconds`
-`You have {} Private Messages`
-`You are in {} Groups`
-`You are in {} Super Groups`
-`You Are in {} Channels`
-`And finally Bots = {}`""".format(ms, u, g, c, bc, b))
+🔒 You have **{}** Private Messages
+👥 You are in **{}** Groups
+👥👥 You are in **{}** Super Groups
+📣 You Are in **{}** Channels
+🤖 Bots = **{}**""".format(ms, u, g, c, bc, b))
 
